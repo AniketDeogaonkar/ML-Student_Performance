6910d5d8f5c1db2a52552e7b9a44970bdc6b69f5
aniketdeogaonkar <aniketdeogaonkar@gmail.com>
2023-05-27 11:09:42 +0530

    EDA MODELTRAINING

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,7 +9,7 @@ from dataclasses import dataclass
 
 @dataclass 
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifact',"train.csv") 
+    train_data_path: str=os.path.join('artifact',"train.csv")
     test_data_path: str=os.path.join('artifact',"test.csv")
     raw_data_path: str=os.path.join('artifact',"raw_data.csv")
     
