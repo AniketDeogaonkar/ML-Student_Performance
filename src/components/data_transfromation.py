0621f881db93b92eb9228cc4d55af5db3db81ab4
aniketdeogaonkar <aniketdeogaonkar@gmail.com>
2023-05-29 19:35:00 +0530

    DATA TRANSFORMATION

--- src/components/data_transfromation.py
+++ src/components/data_transfromation.py
@@ -91,10 +91,10 @@ class DataTransformation:
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
             
             train_arr = np.c_[
-                input_feature_train_arr,np.array(target_feature_train_df)
+            input_feature_train_arr,np.array(target_feature_train_df)
             ]
             test_arr = np.c_[
-                input_feature_test_arr,np.array(target_feature_test_df)
+            input_feature_test_arr,np.array(target_feature_test_df)
             ]
             
             logging.info(f"saved preprocessing object.")
